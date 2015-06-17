d23bf39404a1fc32e978b68e8a43b60d0632f102
marisolat <marisol.acevedo@gmail.com>
2015-06-17 13:41:37 -0500

    Added additional comments.

--- cachematrix.R
+++ cachematrix.R
@@ -9,32 +9,48 @@
 ##                  then `cacheSolve` will retrieve the inverse from the cache.
 ##
 ## Assumptions:
-## ~ supplied matrix is always invertible
+## ~ supplied matrix is always square and invertible
 
-## Create a matrix object that can cache its inverse
+## Create a matrix object env that can cache values
 makeCacheMatrix <- function(x = matrix()) {
-    m <- NULL
+    # Initialize a placeholder for an inverted matrix
+    mInv <- NULL
+    
+    # Assign x & mInv in the enclosed env (env for makeCacheMatrix)
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        mInv <<- NULL
     }
+    
+    # Return the cached value of matrix x
     get_x <- function() x
-    setSolve <- function(solve) m <<- solve
-    getSolve <- function() m
+    
+    # Assign mInv in the enclosed env (env for makeCacheMatrix)
+    setSolve <- function(solve) mInv <<- solve
+    
+    # Return the cached value of mInv
+    getSolve <- function() mInv
+    
+    # Store these functions as list names: access with makeCacheMatrix$name
     list(set = set, get_x = get_x, setSolve = setSolve, getSolve = getSolve)
 }
 
 ## Compute the inverse of a matrix object OR retrieve result if no chg
 cacheSolve <- function(x, ...) {
+    # Retrieve existing value for matrix solution
     m <- x$getSolve()
-    if(!is.null(m)){
+    
     # If matrix solution exists, retrieve
+    if(!is.null(m)){
         message("getting cached data")
         return(m)
     }
-    # Otherwise, calculate that inverse
+    
+    # Otherwise, calculate the inverse for the given matrix
     data <- x$get_x()
     m <- solve(data, ...)
+    
+    # Assign the solution for the given matrix
     x$setSolve(m)
-    m    
+    m   
 }
